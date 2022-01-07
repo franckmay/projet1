03e88b092b55d9c8a51a51fd3e21a865f4ab47d2
Franck Thiam <matterf3@gmail.com>
2022-01-07 15:31:49 +0100

    *ajout du component principal hom avec ses nav et side bars *changement du type de fichiers de style de css en scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,42 @@
 import { NgModule } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
 
+
+/** primeng */
+import { TimelineModule } from 'primeng/timeline';
+import { CardModule } from 'primeng/card';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { OrganizationChartModule } from 'primeng/organizationchart';
+import { BadgeModule } from "primeng/badge";
+import { ReseauComponent } from './components/reseau/reseau.component';
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import {AvatarModule} from 'primeng/avatar';
+import {AvatarGroupModule} from 'primeng/avatargroup';
+
+//components//
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { ProfilComponent } from './components/profil/profil.component';
-
-
-
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
-import {TimelineModule} from 'primeng/timeline';
-import {CardModule} from 'primeng/card';
-import { BadgeModule } from "primeng/badge";
+import { AccueilComponent } from './components/accueil/accueil.component';
 
 
 
 
+
+
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient, 'assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -25,14 +44,33 @@ import { BadgeModule } from "primeng/badge";
     HomeComponent,
     LoginComponent,
     ProfilComponent,
- 
-    
+    ReseauComponent,
+    AccueilComponent,
+
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
-    TimelineModule,CardModule,BadgeModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+
+
+    // primeng
+    TimelineModule, CardModule, BadgeModule, SelectButtonModule, OrganizationChartModule, MessagesModule,
+    MessageModule, RadioButtonModule,AvatarModule,
+    AvatarGroupModule,
+
+
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
